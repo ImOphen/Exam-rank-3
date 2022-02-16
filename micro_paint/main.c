5777e1f05f848d5c183c92aefceb1953b359a150
Ophen <itsOphen@gmail.com>
2022-02-16 09:51:18 +0100

    restore old push

--- micro_paint/main.c
+++ micro_paint/main.c
@@ -1,80 +1,119 @@
-#include <libc.h>
-#define ARG_ERR "Error: argument\n"
-#define ERR "Error: Operation file corrupted\n"
-char **tab = NULL;
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#define ERR_ARG "Error: argument\n"
+#define ERR_FILE "Error: Operation file corrupted\n"
+
+int x;
+int y;
 char c;
-int x, y;
-typedef struct s_elem
+char **tab = NULL;
+
+typedef struct s_element
 {
-	char c, t;
-	float x,w,h,y;
-}t_elem;
-int	ft_strlen(char *str)
+    char t;
+    float x;
+    float y;
+    float w;
+    float h;
+    char c;
+}               t_element;
+
+int ft_strlen(char *str)
 {
-	int i = -1;
-	while (str[++i]);
-	return (i);
+    int i = 0;
+    while (str[i])
+        i++;
+    return i;
 }
-void	ft_putstr(char *s)
+
+void ft_putstr_ln(char *str)
 {
-	write(1, s, ft_strlen(s));
-	write(1, "\n", 1);
+    int i = 0;
+    while (str[i])
+        write(1, &str[i++], 1);
+    write(1, "\n", 1);
 }
-int		check_if_rectangel(t_elem *el, int line, int col)
+
+void error(FILE *fd, int err)
 {
-	if (line < el->y || line > el->y + el->h || col < el->x || col > el->x + el->w)
-		return (0);
-	else if (col - el->x < 1 || (el->x + el->w ) - col < 1 || line - el->y < 1 || (el->y + el->h) - line < 1)
-		return (2);
-	return (1);
+    if (err == 1)
+        write(1, ERR_ARG, ft_strlen(ERR_ARG));
+    else if (err == 2)
+        {
+            write(1, ERR_FILE, ft_strlen(ERR_FILE));
+            if (fd)
+                fclose(fd);
+        }
 }
-int main(int ac, char **av)
+
+int check_if_rectangle(t_element *el,int line,int col)
 {
-	if (ac != 2)
-		return (ft_putstr(ARG_ERR), 1);
-	FILE *fd = NULL;
-	int col, line, res, i = -1;
-	t_elem el;
-	if ((fd = fopen(av[1], "r")))
-	{
-		if ((res = fscanf(fd, "%d %d %c\n", &x, &y, &c)) == 3 && x > 0 && x <= 300 && y > 0 && y <= 300)
-		{
-			tab = (char **)malloc(sizeof(char *) * y + 1);
-			tab[y] = 0;
-			while (++i < y)
-			{
-				tab[i]  = malloc(sizeof(char ) * x + 1);
-				tab[i][x] = 0;
-				memset(tab[i], c, x);
-			}
-		}
-		while (TRUE)
-		{
-			res = fscanf(fd, "%c %f %f %f %f %c\n", &el.t, &el.x, &el.y, &el.w, &el.h, &el.c);
-			if (res == -1)
-			{
-				i = -1;
-				while (tab[++i])
-					ft_putstr(tab[i]);
-				fclose (fd);
-				return (0);
-			}
-			else if (res != 6 || el.w <= 0 || el.h <= 0 || (el.t != 'r' && el.t != 'R'))
-				return (ft_putstr(ERR), 1);
-			else{
-				line = -1;
-				while (++line < y)
-				{
-					col = -1;
-					while (++col < x)
-					{
-						res = check_if_rectangel(&el, line, col);
-						if ((res == 2 && el.t == 'r') || (res && el.t == 'R'))
-							tab[line][col] = el.c;
-					}
-				}
-			}
-		}
-	}
-	return (ft_putstr(ERR), 1);
+    if (line < el->y || line > el->y + el->h || col < el->x || col > el->x + el->w)
+        return 0;
+    if (col - el->x < 1 || (el->x + el->w) - col < 1 || line - el->y < 1 || (el->y +el->h) - line < 1)
+        return 2;
+    return 1;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+        return(error(NULL, 1),1);
+    FILE *fd = NULL;
+    int res;
+    t_element el;
+    int line = 0;
+    int col;
+    int i = 0;
+    if ((fd = fopen(argv[1], "r")))
+    {
+        if ((res = fscanf(fd, "%d %d %c\n", &x, &y, &c)) == 3 && x > 0 && x <= 300 && y > 0 && y <= 300)
+        {
+            tab = malloc(sizeof(char *) * y + 1);
+            tab[y] = 0;
+            while (i < y)
+            {
+                tab[i] = malloc(sizeof(char) * x + 1);
+                tab[i][x] = 0;
+                memset(tab[i], c, x);
+                i++;
+            }
+            while(1)
+            {
+                res = fscanf(fd, "%c %f %f %f %f %c\n", &el.t, &el.x, &el.y, &el.w, &el.h, &el.c);
+                if (res == -1)
+                {
+                    i = 0;
+                    while (tab[i])
+                        ft_putstr_ln(tab[i++]);
+                    fclose(fd);
+                    return 0;
+                }
+                else if (res != 6 || el.w <= 0 || el.h <= 0 || (el.t != 'r' && el.t != 'R'))
+                    return(error(fd, 2), 1);
+                else
+                {
+                    line = 0;
+                    while(line < y)
+                    {
+                        col = 0;
+                        while (col < x)
+                        {
+                            res = check_if_rectangle(&el, line, col);
+                            if (res == 2 && el.t == 'r')
+                                tab[line][col] = el.c;
+                            else if (res && el.t == 'R')
+                                tab[line][col] = el.c;
+                            col++;
+                        }
+                        line++;
+                    }
+                }
+            }
+        }
+    }
+    return(error(fd, 2), 1);
 }
